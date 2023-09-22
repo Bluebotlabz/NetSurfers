cf39a82294223558c3a87bbbe557ff98baf65030
Bluebotlabz <69104218+Bluebotlabz@users.noreply.github.com>
2023-09-22 11:55:41 +0100

    Create actual hell

---
+++ scripts/content.js
@@ -0,0 +1,32 @@
+const body = document.querySelector("body");
+
+// Video ID, Length in seconds
+const videoLinks = [
+  // Subway surfers
+  ["L_fcrOyoWZ8", 3600],
+  ["_bwtEtYQwgc", 39600],
+
+  // Reddit stories
+  ["kiQ2dTfRhpE", 7200],
+
+  // Family guy
+  ["pt_k_HNz1DE", 10500]
+]
+
+// `document.querySelector` may return null if the selector doesn't match anything.
+if (body) {
+  // Support for API reference docs
+  const textElements = body.querySelectorAll("p");
+
+  let timeOffset = 1;
+  for (const textElement of textElements) {
+    let badge = document.createElement("iframe");
+    //badge.textContent = "VIDEO GOES HERE";
+
+    const randomVideo = (Math.random() * (videoLinks.length-1)).toFixed(0)
+
+    badge.src = "https://www.youtube.com/embed/" + videoLinks[randomVideo][0] + "?autoplay=1&mute=1&loop=1&start=" + (Math.random() * videoLinks[randomVideo][1]).toFixed().toString()
+    setTimeout(() => {textElement.insertAdjacentElement("beforeBegin", badge)}, 1500 * timeOffset);
+    timeOffset += 1;
+  }
+}
